3e041052931f593713c7a1802a107193d5d694a4
avivnachman <35191746+avivnachman@users.noreply.github.com>
2018-01-07 19:28:13 +0200

    check
    check

--- cmake-build-debug/CMakeFiles/Wet2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Wet2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Fady/Desktop/Study/Semester3/data structure/Wet2/Wet2/main.cpp" "C:/Users/Fady/Desktop/Study/Semester3/data structure/Wet2/Wet2/cmake-build-debug/CMakeFiles/Wet2.dir/main.cpp.obj"
+  "C:/Users/Aviv/CLionProjects/Wet2/main.cpp" "C:/Users/Aviv/CLionProjects/Wet2/cmake-build-debug/CMakeFiles/Wet2.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main() {
     std::cout << "change2!" << std::endl;
     std::cout << "!" << std::endl;
+    std::cout<<"FML";
+    std::cout<<"FML2";
+    std::cout<<"FML3";
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
